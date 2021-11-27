6c983e9bae12b010ba0aedfc8bed6f0e0b53e57f
ffleita <facundoezequielfleitas96@gmail.com>
2021-11-27 11:33:37 -0300

    primer commit

---
+++ src/test/java/ar/com/storeapp/ffleitas/storeapp/StoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.storeapp.ffleitas.storeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
